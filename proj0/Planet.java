c1755d702d6470f68315e4ed75ad76a5b6c2ce50
highcold <767151866@qq.com>
2019-06-26 01:19:55 +0800

    test proj0 again

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,10 +1,10 @@
 public class Planet {
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    private double xxPos;
+    private double yyPos;
+    private double xxVel;
+    private double yyVel;
+    private double mass;
+    private String imgFileName;
     static final private double  G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img){
