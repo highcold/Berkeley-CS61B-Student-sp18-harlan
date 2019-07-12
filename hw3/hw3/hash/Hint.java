60da39f536c3597a34ea8f45501b6dffc8771da6
highcold <767151866@qq.com>
2019-07-12 21:12:44 +0800

    hw3 version3

--- hw3/hw3/hash/Hint.java
+++ hw3/hw3/hash/Hint.java
@@ -1,7 +1,5 @@
 package hw3.hash;
 
-import edu.princeton.cs.algs4.StdRandom;
-
 public class Hint {
     public static void main(String[] args) {
         System.out.println("The powers of 256 in Java are:");
